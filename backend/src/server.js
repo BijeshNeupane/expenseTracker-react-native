628ba47c1b7d556c5279b76abb0dcf1e0900f849
BijeshNeupane <neubij64@gmail.com>
2025-11-21 21:18:30 +0545

    backend also

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,37 @@
 import express from "express";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-import transactionRoute from "./routes/transaction.route.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
-config();
+dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
 app.use(rateLimiter);
 app.use(express.json());
 
+// our custom simple middleware
+// app.use((req, res, next) => {
+//   console.log("Hey we hit a req, the method is", req.method);
+//   next();
+// });
+
 const PORT = process.env.PORT || 5001;
 
-app.use("/api/transactions", transactionRoute);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
+
+app.use("/api/transactions", transactionsRoute);
 
 initDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+    console.log("Server is up and running on PORT:", PORT);
   });
 });
